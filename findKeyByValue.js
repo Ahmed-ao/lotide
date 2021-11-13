0841945e69f36e36f89fd9169c5568530c9f0e9d
Ahmed Abdallh <Awes.7can@gamil.com>
2021-11-13 18:58:58 +0000

    add findKeyByValue function

---
+++ findKeyByValue.js
@@ -0,0 +1,23 @@
+// Assertion
+const assertEqual = function (actual, expected) {
+	if (actual === expected) {
+		console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+	} else {
+		console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+	}
+};
+// Main function
+const findKeyByValue = (object, value) => {
+	for (const key in object) {
+		if (object[key] === value) {
+			return key;
+		}
+	}
+	return undefined;
+};
+
+// Test Cases
+assertEqual(findKeyByValue({ 1: 'one', 2: 'two', 3: 'three' }, 'one'), '1');
+assertEqual(findKeyByValue({ 1: 'one', 2: 'two', 3: 'three' }, 'three'), '3');
+assertEqual(findKeyByValue({ 1: 'one', 2: 'two', 3: 'three' }, 'three'), '4');
+assertEqual(findKeyByValue({ 1: 'one', 2: 'two', 3: 'three' }, 'four'), '3');
