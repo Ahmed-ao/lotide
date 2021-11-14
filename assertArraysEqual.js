622e000caa2139317cfd32707c76fee25a979b4f
Ahmed Abdallh <Awes.7can@gamil.com>
2021-11-14 15:58:20 +0000

    add assertArraysEqual

---
+++ assertArraysEqual.js
@@ -0,0 +1,21 @@
+// Main
+const assertArraysEqual = function (array, match) {
+	if (
+		Array.isArray(array) &&
+		Array.isArray(match) &&
+		array.length === match.length &&
+		array.every((val, index) => val === match[index])
+	) {
+		console.log(`✅✅✅ Assertion Passed: ${array} === ${match}`);
+	} else {
+		console.log(`🛑🛑🛑 Assertion Failed: ${array} !== ${match}`);
+	}
+};
+
+// Test Cases
+// Sould Pass
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual(['1', '2', '3'], ['1', '2', '3']);
+// Should fail
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual([1, 2, 3], ['1', 2, 3]);
